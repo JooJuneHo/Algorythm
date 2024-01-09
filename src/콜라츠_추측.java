97f738ace07a44255043801bb2b413d99447b693
JooJuneHo <jw059@naver.com>
2024-01-09 09:52:30 +0900

    algo

---
+++ src/콜라츠_추측.java
@@ -0,0 +1,29 @@
+public class 콜라츠_추측 {
+    public static void main(String[] args) {
+        Solution23 solution23 = new Solution23();
+        System.out.println(solution23.solution(1324));
+    }
+}
+
+class Solution23 {
+    public int solution(int num) {
+        int answer = 0;
+
+        while(num!=1){
+            if(num%2 == 0){
+                num /= 2;
+            } else if(num%2 == 1){
+                num = num*3 + 1;
+            }
+            answer++;
+
+            if(answer == 500){
+                answer = -1;
+                break;
+            }
+        }
+
+
+        return answer;
+    }
+}
