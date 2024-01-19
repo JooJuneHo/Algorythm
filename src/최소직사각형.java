47300427e907c2a1441043805f18518345b16519
JooJuneHo <jw059@naver.com>
2024-01-19 09:05:41 +0900

    algo

---
+++ src/최소직사각형.java
@@ -0,0 +1,26 @@
+public class 최소직사각형 {
+    public static void main(String[] args) {
+        Solution44 solution44 = new Solution44();
+        int[][] sizes = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
+        System.out.println(solution44.solution(sizes));
+    }
+}
+
+class Solution44 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int maxLength=0;
+        int maxHeight=0;
+        for(int i=0; i< sizes.length; i++){
+            if(sizes[i][0] > maxLength){
+                maxLength = sizes[i][0];
+            }
+            if(sizes[i][1] > maxHeight){
+                maxHeight = sizes[i][1];
+            }
+        }
+
+
+        return answer;
+    }
+}
