ad0e20e7f532a0ce80bb9cd4f369fa6be8cc46a4
JooJuneHo <jw059@naver.com>
2024-02-02 10:42:24 +0900

    algo

--- src/programmers.java
+++ src/programmers.java
@@ -1,29 +1,14 @@
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class programmers{
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int price = 580000;
-        System.out.println(solution.solution(price));
+        System.out.println(Arrays.toString(solution.solution(12345)));
     }
 }
 
 class Solution {
-    public int solution(int price) {
-        int answer = 0;
-
-
-        if(price >= 500000){
-            price *= 0.8;
-        } else if(price >= 300000){
-            price *= 0.9;
-        } else if(price >= 100000){
-            price *= 0.95;
-        }
-
-        return price;
+    public int[] solution(long n) {
+        return new StringBuilder().append(n).reverse().chars().map(Character::getNumericValue).toArray();
     }
 }
