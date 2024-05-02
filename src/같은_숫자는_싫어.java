efdba625fa03cb8bee5cfe12323fc01a0c80f791
JooJuneHo <jw059@naver.com>
2024-05-02 20:42:49 +0900

    algo

---
+++ src/같은_숫자는_싫어.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+public class 같은_숫자는_싫어 {
+
+    public static void main(String[] args) {
+        PracSolution2 solution = new PracSolution2();
+        int[] arr = {1,1,3,3,0,1,1};
+        System.out.println(Arrays.toString(solution.solution(arr)));
+    }
+}
+
+class PracSolution2 {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(i==0) stack.push(arr[i]);
+            else if(!stack.peek().equals(arr[i])) stack.push(arr[i]);
+        }
+        int[] answer = new int[stack.size()];
+
+        for(int i=stack.size()-1; i>=0; i--){
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
