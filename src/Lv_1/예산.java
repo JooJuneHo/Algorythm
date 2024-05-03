f3ac1cb4ca3ad3e3b5edff8d3022e3e1d25fb4c9
JooJuneHo <jw059@naver.com>
2024-05-03 14:56:57 +0900

    algo

--- src/Lv_1/예산.java
+++ src/Lv_1/예산.java
@@ -1,5 +1,7 @@
 package Lv_1;
 
+import java.util.Arrays;
+
 public class 예산 {
 
     public static void main(String[] args) {
@@ -15,13 +17,13 @@ class PracSolution3 {
 
     public int solution(int[] d, int budget) {
         int answer = 0;
-
+        int total = 0;
+        Arrays.sort(d);
         for (int i = 0; i < d.length; i++) {
-            budget -= d[i];
-            if (budget < 0) {
-                break;
+            if (total + d[i] <= budget) {
+                total += d[i];
+                answer++;
             }
-            answer++;
         }
 
         return answer;
