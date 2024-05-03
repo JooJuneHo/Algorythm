79cdafcc431940af5abaf215d599d545cbe242bc
JooJuneHo <jw059@naver.com>
2024-05-03 14:54:39 +0900

    algo

---
+++ src/Lv_1/예산.java
@@ -0,0 +1,29 @@
+package Lv_1;
+
+public class 예산 {
+
+    public static void main(String[] args) {
+        PracSolution3 solution3 = new PracSolution3();
+        int[] d = {1, 3, 2, 5, 4};
+        int budget = 9;
+        System.out.println(solution3.solution(d, budget));
+    }
+}
+
+
+class PracSolution3 {
+
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+            if (budget < 0) {
+                break;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
