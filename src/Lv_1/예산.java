058dce71cb2124e6c017e48f8ef2a13f8811b902
JooJuneHo <jw059@naver.com>
2024-05-04 16:02:04 +0900

    algo

--- src/Lv_1/예산.java
+++ src/Lv_1/예산.java
@@ -17,13 +17,14 @@ class PracSolution3 {
 
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int total = 0;
+
         Arrays.sort(d);
         for (int i = 0; i < d.length; i++) {
-            if (total + d[i] <= budget) {
-                total += d[i];
-                answer++;
+            budget -= d[i];
+            if (budget < 0) {
+                break;
             }
+            answer++;
         }
 
         return answer;
