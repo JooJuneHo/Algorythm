01b1b2b6041cd9ce73770e4a7c4944b51355b06d
JooJuneHo <jw059@naver.com>
2024-06-01 21:23:31 +0900

    algo

---
+++ src/Lv_2/피보나치수.java
@@ -0,0 +1,22 @@
+package Lv_2;
+
+public class 피보나치수 {
+    public static void main(String[] args) {
+        Solution8 solution8 = new Solution8();
+        System.out.println(solution8.solution(3));
+    }
+}
+
+class Solution8 {
+    public int solution(int n) {
+        int[] answer = new int[n + 1];
+        answer[0] = 0;
+        answer[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            answer[i] = (answer[i - 1] + answer[i - 2]) % 1234567;
+        }
+
+        return answer[n] % 1234567;
+    }
+}
