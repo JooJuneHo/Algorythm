7c449ae6b36ca6485a0c492a653ba2e1853ab1f4
JooJuneHo <jw059@naver.com>
2024-06-25 22:12:54 +0900

    algo

---
+++ src/Lv_0/practice.java
@@ -0,0 +1,18 @@
+package Lv_0;
+
+public class practice {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(15));
+    }
+}
+
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
