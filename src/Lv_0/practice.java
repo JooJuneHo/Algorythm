61ce51ee32295fc30f354deddea87a20b9784267
JooJuneHo <jw059@naver.com>
2024-07-09 18:38:07 +0900

    algo

--- src/Lv_0/practice.java
+++ src/Lv_0/practice.java
@@ -1,18 +1,41 @@
 package Lv_0;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class practice {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(15));
+        System.out.println(solution.solution("AAAAE"));
     }
 }
 
 
 class Solution {
-    public int solution(int n) {
+    static String[] words = {"A", "E", "I", "O", "U"};
+    static List<String> list;
+
+    public int solution(String word) {
         int answer = 0;
+        list = new ArrayList<>();
 
-
+        dfs("", 0);
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
+        }
         return answer;
+    }
+
+    static void dfs(String str, int len) {
+        list.add(str);
+        if (len == 5) return;
+        for (int i = 0; i < 5; i++) {
+            dfs(str + words[i], len + 1);
+        }
     }
 }
---
+++ src/Lv_2/n진수게임.java
@@ -0,0 +1,36 @@
+package Lv_2;
+
+public class n진수게임 {
+    public static void main(String[] args) {
+        Solution29 solution29 = new Solution29();
+        System.out.println(solution29.solution(2, 4, 2, 1));
+    }
+}
+
+class Solution29 {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+
+        //10진수 0부터시작, 총있어야 숫자의 개수
+        int num = 0, len = t * m;
+
+        //n진수로 변환 후 sb에 붙여주기.
+        while (sb.length() < len) {
+            //16진수일 경우 A~F는 소문자로 나오기때문에 대문자로 고쳐주기위한 toUpperCase() 사용한다.
+            String temp = Integer.toString(num, n).toUpperCase();
+            sb.append(temp);
+            num++;
+        }
+
+        //len의 길이만큼만 사용할꺼임.
+        String str = sb.substring(0, len);
+
+        //p번째(인덱스로는 p-1번째)부터 시작해서 m번재 마다 읽어준다.
+        for (int i = p - 1; i < str.length(); i += m) {
+            char ch = str.charAt(i);
+            answer.append(ch);
+        }
+        return answer.toString();
+    }
+}
