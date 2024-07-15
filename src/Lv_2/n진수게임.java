b31df6a388aae16f90152461a277db827605e549
JooJuneHo <jw059@naver.com>
2024-07-15 22:12:55 +0900

    algo

--- src/Lv_2/n진수게임.java
+++ src/Lv_2/n진수게임.java
@@ -9,28 +9,24 @@ public class n진수게임 {
 
 class Solution29 {
     public String solution(int n, int t, int m, int p) {
-        StringBuilder answer = new StringBuilder();
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
 
-        //10진수 0부터시작, 총있어야 숫자의 개수
-        int num = 0, len = t * m;
+        int num = 0;
 
-        //n진수로 변환 후 sb에 붙여주기.
-        while (sb.length() < len) {
-            //16진수일 경우 A~F는 소문자로 나오기때문에 대문자로 고쳐주기위한 toUpperCase() 사용한다.
+        String str = "";
+
+        for (int i = 0; i < t * m; i++) {
             String temp = Integer.toString(num, n).toUpperCase();
-            sb.append(temp);
+            str += temp;
             num++;
         }
 
-        //len의 길이만큼만 사용할꺼임.
-        String str = sb.substring(0, len);
+        String list = str.substring(0, t * m);
 
-        //p번째(인덱스로는 p-1번째)부터 시작해서 m번재 마다 읽어준다.
-        for (int i = p - 1; i < str.length(); i += m) {
-            char ch = str.charAt(i);
-            answer.append(ch);
+        for (int i = p - 1; i < list.length(); i += m) {
+            char c = list.charAt(i);
+            answer += c;
         }
-        return answer.toString();
+        return answer;
     }
 }
